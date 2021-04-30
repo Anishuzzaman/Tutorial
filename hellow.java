6f55ec5d98508f4b77019b20c3b8785fa03be6eb
Anishuzzaman <aniszamancse@gmail.com>
2021-04-30 15:40:18 +0600

    Change method

--- hellow.java
+++ hellow.java
@@ -1,6 +1,8 @@
-public class hellow{
+public class hellow {
     public static void main(String[] args){
         System.out.println("Visual code is awesome");
     }
+
+    greeting ("git");
 	system.out.println("Hellow Java");
 }
